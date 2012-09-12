2e49eac72ef7f6ed155e0793a8a3d5d3126e6808
Manuel Naujoks <mnaujoks@gmail.com>
2012-09-12 16:23:52 +0200

    but now with source :)

---
+++ Sources/MStrings/ContentScraper.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace MStrings
+{
+    public class ContentScraper
+    {
+        public static ContentScraper For(string expression)
+        {
+            return new ContentScraper(expression);
+        }
+
+        public static implicit operator string(ContentScraper content)
+        {
+            return content.Result;
+        }
+
+        private ContentScraper(string expression)
+        {
+            this._Expression = expression;
+            this.IgnoreCase = true;
+        }
+
+        private string _Expression;
+        private string Expression { get { return this._Expression; } }
+        public bool IgnoreCase { private get; set; }
+
+        private RegexOptions GetOptions()
+        {
+            return this.IgnoreCase ? RegexOptions.IgnoreCase : RegexOptions.None;
+        }
+
+        #region Operating
+        public IEnumerable<ContentScraper> Split(string pattern)
+        {
+            return from s in Regex.Split(this.Expression, pattern, this.GetOptions())
+                   select new ContentScraper(s);
+        }
+
+        public ContentScraper SplitOut(string pattern, Func<IEnumerable<ContentScraper>, int> indexer)
+        {
+            var splitted = this.Split(pattern);
+            return splitted.Skip(indexer(splitted)).First();
+        }
+
+        public ContentScraper SplitOut(string pattern, int index)
+        {
+            return SplitOut(pattern, (s) => index);
+        }
+
+        public bool Matches(string pattern)
+        {
+            return Regex.IsMatch(this.Expression, pattern, this.GetOptions());
+        }
+
+        public ContentScraper Replace(string pattern, string replacement)
+        {
+            return new ContentScraper(Regex.Replace(this.Expression, pattern, replacement, this.GetOptions()));
+        }
+        #endregion
+
+        #region Resulting
+        public string Result
+        {
+            get { return this.Expression.Trim(); }
+        }
+
+        public int ResultAsInt
+        {
+            get { return Int32.Parse(this.Expression.Trim()); }
+        }
+
+        public ContentScraper Until(int index)
+        {
+            if (index < 0) index = this.Expression.Length;
+            return new ContentScraper(this.Expression.Substring(0, index));
+        }
+
+        public ContentScraper Until(string p)
+        {
+            return Until(this.Expression.IndexOf(p));
+        }
+
+        public ContentScraper UntilSecond(string p)
+        {
+            return Until(this.Expression.IndexOf(p, this.Expression.IndexOf(p) + p.Length));
+        }
+
+        public ContentScraper UntilLast(string p)
+        {
+            return Until(this.Expression.LastIndexOf(p));
+        }
+
+        public ContentScraper UntilEarliest(string p1, string p2)
+        {
+            int untilIndex = 0;
+            if (this.Expression.Contains(p1) && this.Expression.Contains(p2))
+                untilIndex = Math.Min(this.Expression.IndexOf(p1), this.Expression.IndexOf(p2));
+            else
+                untilIndex = Math.Max(this.Expression.IndexOf(p1), this.Expression.IndexOf(p2));
+            return new ContentScraper(this.Expression.Substring(0, untilIndex));
+        }
+
+        public ContentScraper StartingAt(string p)
+        {
+            return StartingAt(this.Expression.IndexOf(p));
+        }
+
+        public ContentScraper StartingAtLast(string p)
+        {
+            return StartingAt(this.Expression.LastIndexOf(p));
+        }
+
+        public ContentScraper StartingAt(int p)
+        {
+            return new ContentScraper(this.Expression.Substring(p));
+        }
+
+        public ContentScraper StartingAfter(string p)
+        {
+            return this.StartingAt(p).StartingAt(p.Length);
+        }
+
+        public ContentScraper StartingAfterLast(string p)
+        {
+            return this.StartingAtLast(p).StartingAt(p.Length);
+        }
+
+        public ContentScraper ShortenTo(int p)
+        {
+            return new ContentScraper(this.Expression.Substring(0, p));
+        }
+        #endregion
+    }
+}
---
+++ Sources/MStrings/StringExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace MStrings
+{
+    public static class StringExtensions
+    {
+        public static byte[] ToAscii(this string s)
+        {
+            byte[] retval = new byte[s.Length];
+            for (int ix = 0; ix < s.Length; ++ix)
+            {
+                char ch = s[ix];
+                if (ch <= 0x7f) retval[ix] = (byte)ch;
+                else retval[ix] = (byte)'?';
+            }
+            return retval;
+        }
+
+        public static string Base64ize(this string original)
+        {
+            return Convert.ToBase64String(
+                Encoding.UTF8.GetBytes(original));
+        }
+
+        public static string UnBase64ize(this string b64)
+        {
+            byte[] orgBytes = Convert.FromBase64String(b64);
+            return Encoding.UTF8.GetString(orgBytes, 0, orgBytes.Length);
+        }
+
+        public static string UnWebify(this string s)
+        {
+            var exp = ContentScraper.For(s)
+                .Replace("<.*?>", "")
+                .Replace("\r", "")
+                .Replace("\n", "")
+                .Replace("&quot;", "\"")
+                .Replace("&nbsp;", "")
+                .Until("<");
+            return exp.Result.Trim();
+        }
+
+        public static string NormWhiteSpaces(this string s)
+        {
+            string normedDesc = s;
+            while (normedDesc.Contains("  ")) normedDesc = normedDesc.Replace("  ", " ");
+            return normedDesc.Trim();
+        }
+    }
+}
